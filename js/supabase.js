89994a465454b8b01f6658dacca6ee06ee79312c
businessmoh199-cell <businessmoh199@gmail.com>
2026-06-30 07:13:06 +0100

    Add files via upload

---
+++ js/supabase.js
@@ -0,0 +1,8 @@
+const supabaseUrl = "https://cstplfndguntgaczyupj.supabase.co";
+
+const supabaseKey = "sb_publishable_1lYkdVriUeVBtFWG7dO0hg_YFaIWDr4";
+
+const supabase = window.supabase.createClient(
+  supabaseUrl,
+  supabaseKey
+);
