975ba164c204bc3823131626b96b23327a463088
businessmoh199-cell <businessmoh199@gmail.com>
2026-06-30 11:48:57 +0100

    Rename supabase client variable to 'sb'

--- js/supabase.js
+++ js/supabase.js
@@ -2,7 +2,7 @@ const supabaseUrl = "https://cstplfndguntgaczyupj.supabase.co";
 
 const supabaseKey = "sb_publishable_1lYkdVriUeVBtFWG7dO0hg_YFaIWDr4";
 
-window.supabase = window.supabase.createClient(
+window.sb = window.supabase.createClient(
   supabaseUrl,
   supabaseKey
 );
